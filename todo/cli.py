16ea6fb1b3efd702ec2fd8b576782000e839de25
Christo Olivier <mail@christoolivier.com>
2021-02-20 20:45:46 +0000

    cleaned up unnecessary files.

--- todo/cli.py
+++ todo/cli.py
@@ -1,5 +1,5 @@
 import sys
-from datetime import datetime
+from datetime import date, datetime
 
 from sqlalchemy import engine
 from sqlalchemy.orm import sessionmaker
@@ -18,14 +18,26 @@ session = DBSession()
 
 
 def _convert_to_date(date_str: str):
+    """
+    Convert dates in string format to python date objects.
+    """
     return datetime.strptime(date_str, "%Y-%m-%d").date()
 
 
 def _print_item(item: Item):
-    print(f"Name: {item.name}, Due: {item.due}, Priority: {item.priority}")
+    """
+    Print a todo item's properties to the command line.
+    """
+    print(
+        f"Name: {item.name}, Due: {item.due}, Priority: {item.priority},"
+        f"Status: {item.status}, Completed: {item.completed}"
+    )
 
 
 def _get_args():
+    """
+    Get the arguments passed to the application from the command line.
+    """
     if len(sys.argv) < 2:
         print("Please specify your command.")
         sys.exit(1)
@@ -36,6 +48,9 @@ def _get_args():
 
 
 def add_item(name: str, due: str, priority: str) -> None:
+    """
+    Add a todo item to the database.
+    """
     try:
         new_todo = Item(name=name, due=_convert_to_date(due), priority=priority.lower())
         session.add(new_todo)
@@ -45,7 +60,24 @@ def add_item(name: str, due: str, priority: str) -> None:
         session.rollback()
 
 
+def mark_complete(name: str) -> None:
+    """
+    Mark a todo item as completed.
+    """
+    item = session.query(Item).filter(Item.name == name).first()
+
+    if not item:
+        print("No todo item with name `{name}` found.")
+
+    item.status = "complete"
+    item.completed = datetime.now().date()
+    session.commit()
+
+
 def remove_item(name: str) -> None:
+    """
+    Remove a todo item from the database.
+    """
     item = session.query(Item).filter(Item.name == name).first()
 
     if not item:
@@ -56,23 +88,19 @@ def remove_item(name: str) -> None:
     session.commit()
 
 
-def update_item(name: str, due: str, priority: str) -> None:
-    try:
-        item = session.query(Item).filter_by(name=name).first()
-        item.due = _convert_to_date(due)
-        item.priority = priority
-        session.commit()
-    except AttributeError:
-        print(f"No todo item with name `{name}` found.")
-
-
 def show_all() -> None:
+    """
+    Show all of the items in the database.
+    """
     items = session.query(Item).all()
     for item in items:
         _print_item(item)
 
 
 def show_due(due: str) -> None:
+    """
+    Show all of the items that are due on a specific date.
+    """
     if due.lower() == "today":
         due = datetime.now().date()
     else:
@@ -83,25 +111,46 @@ def show_due(due: str) -> None:
         _print_item(item)
 
 
+def update_item(name: str, due: str, priority: str) -> None:
+    """
+    Update an item's properties.
+    """
+    try:
+        item = session.query(Item).filter_by(name=name).first()
+        item.due = _convert_to_date(due)
+        item.priority = priority
+        session.commit()
+    except AttributeError:
+        print(f"No todo item with name `{name}` found.")
+
+
 def main():
+    """
+    Main application loop.
+    """
     args = _get_args()
 
+    # Get the command from the commandline arguments.
     command = args[0].lower()
 
+    # Execute commands
     if command == "add":
         add_item(args[1], args[2], args[3])
 
-    if command == "update":
-        update_item(args[1], args[2], args[3])
+    if command == "all":
+        show_all()
 
-    if command == "remove":
-        remove_item(args[1])
+    if command == "complete":
+        mark_complete(args[1])
 
     if command == "due":
         show_due(args[1])
 
-    if command == "all":
-        show_all()
+    if command == "remove":
+        remove_item(args[1])
+
+    if command == "update":
+        update_item(args[1], args[2], args[3])
 
 
 if __name__ == "__main__":
--- todo/data.py
+++ todo/data.py
@@ -10,4 +10,6 @@ class Item(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False, unique=True)
     due = Column(Date, nullable=False)
+    completed = Column(Date, nullable=True)
     priority = Column(String(10), nullable=True)
+    status = Column(String(25), default="active", nullable=True)
