5f9f431834c7d2250eaac5afa73872dd5851dd2d
Christo Olivier <mail@christoolivier.com>
2021-02-20 20:21:16 +0000

    cleaned up unnecessary files.

--- todo/data.py
+++ todo/data.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.expression import null
 
 Base = declarative_base()
 
