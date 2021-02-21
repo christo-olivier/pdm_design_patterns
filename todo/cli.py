a246033bb65de771410c2d1b541a00165af4244d
Christo Olivier <mail@christoolivier.com>
2021-02-21 13:06:40 +0000

    re-arranged the application logic.

--- todo/cli.py
+++ todo/cli.py
@@ -1,35 +1,6 @@
 import sys
-from datetime import date, datetime
 
-from sqlalchemy import engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
-
-from todo.data import Base, Item
-
-db_path = "/Users/christo/repos/todo/todo_app.sqlite"
-engine = engine.create_engine(f"sqlite:///{db_path}")
-Base.metadata.create_all(engine)
-DBSession = sessionmaker(bind=engine)
-
-session = DBSession()
-
-
-def _convert_to_date(date_str: str):
-    """
-    Convert dates in string format to python date objects.
-    """
-    return datetime.strptime(date_str, "%Y-%m-%d").date()
-
-
-def _print_item(item: Item):
-    """
-    Print a todo item's properties to the command line.
-    """
-    print(
-        f"Name: {item.name}, Due: {item.due}, Priority: {item.priority},"
-        f"Status: {item.status}, Completed: {item.completed}"
-    )
+import todo.data as data
 
 
 def _get_args():
@@ -45,83 +16,6 @@ def _get_args():
     return args
 
 
-def add_item(name: str, due: str, priority: str) -> None:
-    """
-    Add a todo item to the database.
-    """
-    try:
-        new_todo = Item(name=name, due=_convert_to_date(due), priority=priority.lower())
-        session.add(new_todo)
-        session.commit()
-    except IntegrityError:
-        print("Todo item already exists.")
-        session.rollback()
-
-
-def mark_complete(name: str) -> None:
-    """
-    Mark a todo item as completed.
-    """
-    item = session.query(Item).filter(Item.name == name).first()
-
-    if not item:
-        print("No todo item with name `{name}` found.")
-
-    item.status = "complete"
-    item.completed = datetime.now().date()
-    session.commit()
-
-
-def remove_item(name: str) -> None:
-    """
-    Remove a todo item from the database.
-    """
-    item = session.query(Item).filter(Item.name == name).first()
-
-    if not item:
-        print(f"No todo item with name `{name}` found.")
-        return
-
-    session.delete(item)
-    session.commit()
-
-
-def show_all() -> None:
-    """
-    Show all of the items in the database.
-    """
-    items = session.query(Item).all()
-    for item in items:
-        _print_item(item)
-
-
-def show_due(due: str) -> None:
-    """
-    Show all of the items that are due on a specific date.
-    """
-    if due.lower() == "today":
-        due = datetime.now().date()
-    else:
-        due = _convert_to_date(due)
-
-    items = session.query(Item).filter(Item.due <= due)
-    for item in items:
-        _print_item(item)
-
-
-def update_item(name: str, due: str, priority: str) -> None:
-    """
-    Update an item's properties.
-    """
-    try:
-        item = session.query(Item).filter_by(name=name).first()
-        item.due = _convert_to_date(due)
-        item.priority = priority
-        session.commit()
-    except AttributeError:
-        print(f"No todo item with name `{name}` found.")
-
-
 def main():
     """
     Main application loop.
@@ -133,22 +27,22 @@ def main():
 
     # Execute commands
     if command == "add":
-        add_item(args[1], args[2], args[3])
+        data.add_item(args[1], args[2], args[3])
 
     if command == "all":
-        show_all()
+        data.show_all()
 
     if command == "complete":
-        mark_complete(args[1])
+        data.mark_complete(args[1])
 
     if command == "due":
-        show_due(args[1])
+        data.show_due(args[1])
 
     if command == "remove":
-        remove_item(args[1])
+        data.remove_item(args[1])
 
     if command == "update":
-        update_item(args[1], args[2], args[3])
+        data.update_item(args[1], args[2], args[3])
 
 
 if __name__ == "__main__":
--- todo/data.py
+++ todo/data.py
@@ -1,15 +1,111 @@
-from sqlalchemy import Column, Date, Integer, String
+from datetime import datetime
+
+from sqlalchemy import engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+from todo.model import Base, Item
+
+db_path = "/Users/christo/repos/todo/todo_app.sqlite"
+engine = engine.create_engine(f"sqlite:///{db_path}")
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
 
 
-class Item(Base):
-    __tablename__ = "item"
+def _convert_to_date(date_str: str):
+    """
+    Convert dates in string format to python date objects.
+    """
+    return datetime.strptime(date_str, "%Y-%m-%d").date()
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False, unique=True)
-    due = Column(Date, nullable=False)
-    completed = Column(Date, nullable=True)
-    priority = Column(String(10), nullable=True)
-    status = Column(String(25), default="active", nullable=True)
+
+def _print_item(item: Item):
+    """
+    Print a todo item's properties to the command line.
+    """
+    print(
+        f"Name: {item.name}, Due: {item.due}, Priority: {item.priority}, "
+        f"Status: {item.status}, Completed: {item.completed}"
+    )
+
+
+def add_item(name: str, due: str, priority: str) -> None:
+    """
+    Add a todo item to the database.
+    """
+    try:
+        new_todo = Item(name=name, due=_convert_to_date(due), priority=priority.lower())
+        session.add(new_todo)
+        session.commit()
+    except IntegrityError:
+        print("Todo item already exists.")
+        session.rollback()
+
+
+def mark_complete(name: str) -> None:
+    """
+    Mark a todo item as completed.
+    """
+    item = session.query(Item).filter(Item.name == name).first()
+
+    if not item:
+        print("No todo item with name `{name}` found.")
+
+    item.status = "complete"
+    item.completed = datetime.now().date()
+    session.commit()
+
+
+def remove_item(name: str) -> None:
+    """
+    Remove a todo item from the database.
+    """
+    item = session.query(Item).filter(Item.name == name).first()
+
+    if not item:
+        print(f"No todo item with name `{name}` found.")
+        return
+
+    session.delete(item)
+    session.commit()
+
+
+def show_all() -> None:
+    """
+    Show all of the items in the database.
+    """
+    items = session.query(Item).all()
+    for item in items:
+        _print_item(item)
+
+
+def show_due(due: str) -> None:
+    """
+    Show all of the items that are due on a specific date.
+    """
+    if due.lower() == "today":
+        due = datetime.now().date()
+    else:
+        due = _convert_to_date(due)
+
+    items = session.query(Item).filter(Item.due <= due)
+    for item in items:
+        _print_item(item)
+
+
+def update_item(name: str, due: str, priority: str) -> None:
+    """
+    Update an item's properties.
+    """
+    try:
+        item = session.query(Item).filter_by(name=name).first()
+        item.due = _convert_to_date(due)
+        item.priority = priority
+        item.status = "active"
+        item.completed = None
+        session.commit()
+    except AttributeError:
+        print(f"No todo item with name `{name}` found.")
